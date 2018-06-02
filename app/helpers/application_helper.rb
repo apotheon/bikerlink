71c39e23fb76fe7d624a4ad7a22f4e0c8f5c7a5a
ren <code@apotheon.net>
2018-06-02 21:33:20 +0000

    add Devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def devise_mapping
+    Devise.mappings[:biker]
+  end
+
+  def resource_name
+    devise_mapping.name
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
 end
