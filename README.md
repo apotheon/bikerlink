6a71d7de2b8fb00d3ed48eb96d00593c2ac5d334
ren <ren@noemail.net>
2018-06-02 09:18:54 +0000

    create basic application
    FossilOrigin-Name: 3c61b92582ae3449f55f22f5fb3074a7f3915430

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Biker Link
 
 a place to link bikers together on the internet
+
+## Versions
+
+Ruby 2.3.5
+
+Rails 4.0.0
+
+## Testing
+
+    $ be rspec
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bikerlink::Application.config.session_store :cookie_store, key: '_bikerlink_session'
