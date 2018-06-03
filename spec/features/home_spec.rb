b9d65e217c942f334f59afa445d56526c32b3e3a
ren <ren@noemail.net>
2018-06-03 00:06:34 +0000

    add account links to homepage
    FossilOrigin-Name: c89627a1ac890d8fcb1b941b6fdd437dc12fd151

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Home' do
   context 'when a user visits the home page' do
     before :each do
-      visit '/'
+      visit root_path
     end
 
     scenario 'identify the site' do
@@ -14,9 +14,22 @@ RSpec.feature 'Home' do
       expect(page).to have_text 'connecting riders online'
     end
 
-    scenario 'show login/signup links' do
+    scenario 'show sign in/up links' do
       expect(page).to have_link 'Sign In', href: '/bikers/sign_in'
       expect(page).to have_link 'Sign Up', href: '/bikers/sign_up'
+    end
+
+    context 'while signed in' do
+      before :each do
+        @biker = create :biker
+        sign_in @biker
+        visit root_path
+      end
+
+      scenario 'show account links' do
+        expect(page).to have_link 'Sign Out', href: '/bikers/sign_out'
+        expect(page).to have_link 'Account Settings', href: '/bikers/edit'
+      end
     end
   end
 end
