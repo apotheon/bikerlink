6ca44e3fb1aac5d744f423d15489fb717d73abee
ren <code@apotheon.net>
2018-06-08 01:44:46 +0000

    turn on exceptions in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Bikerlink::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_dispatch.show_exceptions = true
 end
