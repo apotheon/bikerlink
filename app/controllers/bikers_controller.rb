9db4ce7045c5770c7708d23e7a9f9883fe53babc
ren <ren@noemail.net>
2018-06-15 20:43:49 +0000

    sort bikers in list view; eliminate premature before_action
    FossilOrigin-Name: ab2d3ba760fe1f315467813d7ca55b55fa75c11f

--- app/controllers/bikers_controller.rb
+++ app/controllers/bikers_controller.rb
@@ -1,15 +1,14 @@
 class BikersController < ApplicationController
-  before_action :find_biker, only: [:show]
-
   def index
     if current_biker.admin
-      @bikers = Biker.all
+      @bikers = Biker.all.sort_by {|b| b.username }
     else
       redirect_to root_path, alert: 'You are not authorized for that action.'
     end
   end
 
   def show
+    find_biker
   end
 
   private
