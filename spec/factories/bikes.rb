304b12d7f9117b6b2242888613dfc7c681235972
ren <code@apotheon.net>
2018-06-21 21:31:33 +0000

    add association to bike factory

--- spec/factories/bikes.rb
+++ spec/factories/bikes.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :bike do
     name 'Vroom Vroom'
     description 'It is very fast!'
+    association :owner, factory: :biker
 
     trait :sheila do
       name 'Sheila'
