af2096ecbeed06d7dd4a45fef6f729a3364dea91
ren <ren@noemail.net>
2018-06-21 21:31:33 +0000

    add association to bike factory
    FossilOrigin-Name: f619747ece06a58d2cc55594fd29f2dbfbb727a5

--- spec/features/bike_spec.rb
+++ spec/features/bike_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.feature 'Bike' do
-  before :each do
+  scenario 'identify one owner for a bike' do
     @biker = create :biker
     @bike = create :bike, owner_id: @biker.id
+    expect(@bike.owner.username).to eq @biker.username
   end
 
-  scenario 'identify one owner for a bike' do
-    expect(@bike.owner.username).to eq @biker.username
+  context 'with existing bikes' do
+    before :each do
+      @vroom = create :bike
+      @sheila = create :bike, :sheila, owner_id: @vroom.owner_id
+    end
+
+    scenario 'owner has several bikes' do
+      expect(@vroom.owner.bikes.size).to eq 2
+      expect(@vroom.owner.bikes.first.name).to eq 'Vroom Vroom'
+      expect(@vroom.owner.bikes.last.name).to eq 'Sheila'
+    end
   end
 end
