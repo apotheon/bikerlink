e5a2eacdda06da4f59dda9f25b675b90e1857e73
ren <ren@noemail.net>
2018-06-22 01:24:14 +0000

    add minimal bike creation functionality
    FossilOrigin-Name: f67619ed9c869a98ff1f1c9059678d8446d70671

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,29 @@
+class BikesController < ApplicationController
+  def new
+    @bike = Bike.new
+  end
+
+  def edit
+    @bike = find_bike
+  end
+
+  def create
+    @bike = current_biker.bikes.build bike_params
+
+    if @bike.save
+      redirect_to edit_bikes_path @bike.name
+    else
+      render home_path, alert: 'Failed To Add Bike'
+    end
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:name)
+  end
+
+  def find_bike
+    @bike = Bike.find_by_name(params[:format])
+  end
+end
